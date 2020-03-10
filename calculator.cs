cc02bb6ee953ea05af329c1efeca6d7dd0654a30
aryafin <arya.yusuf@students.amikom.ac.id>
2020-03-10 12:13:37 +0700

    tahap kedua

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,23 @@ class calculator
 {
 	static void main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		console.writeline("hasil penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+		console.writeline("hasil pengurangan: {0} - {1} = {2}", a, b, penambahan(a, b));
+		
+		console.Writeline("\nTekan sak karepmu ben metu");
+		consule.readkey();
+	}
 	
+	static int penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int pengurangan(int a, int b)
+	{
+		return a-b;
 	}
 } 
